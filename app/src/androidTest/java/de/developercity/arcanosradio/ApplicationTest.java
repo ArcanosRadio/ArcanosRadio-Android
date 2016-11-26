7c3b5e9b17a01ccc680dbc7b2c301a6ec6a43c72
Luiz Barbosa <luiz.barbosa@here.com>
2016-11-26 02:03:41 +0100

    Play Store release

--- app/src/androidTest/java/de/developercity/arcanosradio/ApplicationTest.java
+++ app/src/androidTest/java/de/developercity/arcanosradio/ApplicationTest.java
@@ -3,11 +3,24 @@ package de.developercity.arcanosradio;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    @ClassRule public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void takeScreenshots() {
+        Screengrab.screenshot("name_of_screenshot_here");
     }
 }
